b956b802a039d1e04d1b9b0027719871d5006ad7
Honghao Li <im.lihh@outlook.com>
2020-12-02 23:36:29 +0800

    2

--- examples/hello-redis.rs
+++ examples/hello-redis.rs
@@ -1,14 +1,55 @@
+use bytes::Bytes;
 use mini_redis::{client, Result};
+use tokio::sync::mpsc;
+
+#[derive(Debug)]
+enum Command {
+    Get { key: String },
+    Set { key: String, val: Bytes },
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let (mut tx, mut rx) = mpsc::channel(32);
+    let mut tx2 = tx.clone();
+
     let mut client = client::connect("127.0.0.1:6379").await?;
 
-    client.set("hello", "world".into()).await?;
+    let t1 = tokio::spawn(async move {
+        let cmd = Command::Get {
+            key: "hello".to_string(),
+        };
 
-    let result = client.get("hello").await?;
+        tx.send(cmd).await.unwrap();
+    });
 
-    println!("Got value from the server, result={:?}", result);
+    let t2 = tokio::spawn(async move {
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+        };
+
+        tx2.send(cmd).await.unwrap();
+    });
+
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
+            match dbg!(cmd) {
+                Get { key } => {
+                    client.get(&key).await;
+                }
+                Set { key, val } => {
+                    client.set(&key, val).await;
+                }
+            }
+        }
+    });
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
 
     Ok(())
 }
